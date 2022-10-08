aa5fb1defb88cfb521846a13b27c39f9cb97f201
tanaysrivastav1 <73322183+tanaysrivastav1@users.noreply.github.com>
2022-10-08 15:59:19 -0500

    new

--- src/App.jsx
+++ src/App.jsx
@@ -9,9 +9,9 @@ const App = () => {
       <h2>
       <button type="button" onclick="alert('Button')">This is a button</button>
       <form>
-      <label for="fname">Insert text</label>
-      <input type="text" id="fname" name="fname">
-        </form>
+        <label for="fname">Insert text</label>
+        <input type="text" id="fname" name="fname"/>
+      </form>
       </h2>
     </div>
   ); 
