10b4f8d4e9eb4b1a1f2728f7014182144874d710
OwenJanssen <67287024+OwenJanssen@users.noreply.github.com>
2022-10-15 13:13:24 -0500

    Form Updating

--- src/components/CreateElection.jsx
+++ src/components/CreateElection.jsx
@@ -4,7 +4,9 @@ import FormControlLabel from '@mui/material/FormControlLabel';
 import FormLabel from '@mui/material/FormLabel';
 import Radio from '@mui/material/Radio';
 import { Button } from "@mui/material";
+import { Position } from './classes';
 import { useDbUpdate } from "../utilities/firebase";
+import { useState } from "react";
 
 const CreateElection = (data) => {
     console.log(data);
@@ -12,20 +14,30 @@ const CreateElection = (data) => {
     //This function can be used for writing database. If the path is a new one, it will create a new record in database. If not, it will update the existing record.
     //Therfore it can be used for adding new item or updating item.
     const [update, result] = useDbUpdate(`/elections/234`);
+    const [positions, setPositions] = useState([new Position("President")]);
 
-    const AddCandidate = () => {
-        console.log("added candidate")
-        return 
-        <div className='createElectionField'>
-            <label className="CreateElectionFieldsText">Candidate:</label>
-            <input type="text" name="candidate"/>
-        </div>
-    }
+    const addPosition = (p) => {
+        setPositions([...positions, p])
+    };
+
+    const modifyPosition = (p) => {
+        const positionsCopy = [...positions];
+        setPositions([...positionsCopy.filter(pos => pos.role !== p.role), p]);
+    };
+
+    // const AddCandidate = () => {
+    //     console.log("added candidate")
+    //     return 
+    //     <div className='createElectionField'>
+    //         <label className="CreateElectionFieldsText">Candidate:</label>
+    //         <input type="text" name="candidate"/>
+    //     </div>
+    // };
 
     const GoToResult = (event) => {
         event.preventDefault();
         window.location.href = "/result";
-    }
+    };
 
     // Here is the example of how to call the update fuction defined above.
     //TODO: need to change to values that users enter
@@ -41,7 +53,7 @@ const CreateElection = (data) => {
         <div className="create-election-container">
             
             <form className="create-election-fields">
-                <div className="position">
+                {/* <div className="position">
                     <label className="text">Position: </label>
                     <input type="text" name="position"/>
                 </div>
@@ -58,7 +70,11 @@ const CreateElection = (data) => {
                     </div>
 
                     <button className="btn btn-dark btn-rounded btn-sm" style={{marginBottom: "20px"}} onClick={AddCandidate}>Add Candidate</button>
-                </div>
+                </div> */}
+
+                {positions.map(p => 
+                    <div>{p.role}</div>
+                )}
 
                 <button className="btn btn-dark btn-rounded" style={{paddingRight:"100px", paddingLeft:"100px", marginBottom: "20px"}} >Add Position</button>
                 <button className="btn btn-success btn-rounded btn-lg" onClick={GoToResult}>Start Election</button>
---
+++ src/components/classes.js
@@ -0,0 +1,6 @@
+export class Position {
+    constructor(role) {
+      this.role = role;
+      this.candidates = [];
+    }
+  }
